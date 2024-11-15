93f01d032664af7e909331db6b6915667aa54d09
nagaraju onteru <nagaraju_onteru@epam.com>
2024-11-15 16:16:29 +0530

    Basic Project created

---
+++ Module1Work/Module1Work/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Module1Work.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
